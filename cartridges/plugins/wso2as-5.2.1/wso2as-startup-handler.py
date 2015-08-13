dc657eae758b3ce82600f07133a6da23ac109514
Punnadi <punnadi@wso2.com>
2015-08-13 15:24:07 +0530

    custom changes

--- cartridges/plugins/wso2as-5.2.1/wso2as-startup-handler.py
+++ cartridges/plugins/wso2as-5.2.1/wso2as-startup-handler.py
@@ -16,81 +16,81 @@
 #
 # ------------------------------------------------------------------------
 
-import mdsclient
+
 from plugins.contracts import ICartridgeAgentPlugin
-from xml.dom.minidom import parse
-import socket
 from modules.util.log import LogFactory
-import time
 import subprocess
 import os
+import psutil
 
 class WSO2ASStartupHandler(ICartridgeAgentPlugin):
+    log = LogFactory().get_log(__name__)
+    log.info("Disabling git ssl verification...")
+    os.environ["GIT_SSL_NO_VERIFY"] = "1"
 
     def run_plugin(self, values):
-        log = LogFactory().get_log(__name__)
-
-        log.info("Disabling git ssl verification...")
-        os.environ["GIT_SSL_NO_VERIFY"] = "1"
-
-        log.info("Reading port mappings...")
-        port_mappings_str = values["PORT_MAPPINGS"]
+        WSO2ASStartupHandler.log.info("Reading port mappings...")
+        port_mappings_str = values["PORT_MAPPINGS"].replace("'", "")
 
         http_proxy_port = None
         https_proxy_port = None
         pt_http_port = None
         pt_https_port = None
 
-        # port mappings format: """NAME:mgt-console|PROTOCOL:https|PORT:4500|PROXY_PORT:9443;
-        #                          NAME:pt-http|PROTOCOL:http|PORT:4501|PROXY_PORT:7280;
+        # port mappings format: """NAME:mgt-console|PROTOCOL:https|PORT:30000|PROXY_PORT:9443;
+        #                          NAME:pt-http|PROTOCOL:http|PORT:30001|PROXY_PORT:7280;
         #                          NAME:pt-https|PROTOCOL:https|PORT:4502|PROXY_PORT:7243"""
-        
-        log.info("Port mappings: %s" % port_mappings_str)
+
+        WSO2ASStartupHandler.log.info("Port mappings: %s" % port_mappings_str)
         if port_mappings_str is not None:
 
             port_mappings_array = port_mappings_str.split(";")
             if port_mappings_array:
 
                 for port_mapping in port_mappings_array:
-                    log.debug("port_mapping: %s" % port_mapping)
+                    WSO2ASStartupHandler.log.debug("port_mapping: %s" % port_mapping)
                     name_value_array = port_mapping.split("|")
                     name = name_value_array[0].split(":")[1]
                     protocol = name_value_array[1].split(":")[1]
-                    port = name_value_array[2].split(":")[1]
-                    if name == "http-9763" and protocol == "http":
-                        http_proxy_port = port
-                    if name == "http-9443" and protocol == "https":
-                        https_proxy_port = port
 
-        log.info("Catalina http proxy port: %s" % http_proxy_port)
-        log.info("Catalina https proxy por: %s" % https_proxy_port)
+                    proxy_port = name_value_array[3].split(":")[1]
+                    # If PROXY_PORT is not set,
+                    if proxy_port == '0':
+                        proxy_port = name_value_array[2].split(":")[1]
+                    if name == "http-9763" and protocol == "http":
+                        http_proxy_port = proxy_port
+                    if name == "http-9443" and protocol == "https":
+                        https_proxy_port = proxy_port
+
+        WSO2ASStartupHandler.log.info("Catalina http proxy port: %s" % http_proxy_port)
+        WSO2ASStartupHandler.log.info("Catalina https proxy por: %s" % https_proxy_port)
 
         if http_proxy_port is not None:
             command = "sed -i \"s/^#CONFIG_PARAM_HTTP_PROXY_PORT.*/CONFIG_PARAM_HTTP_PROXY_PORT = %s/g\" %s" % (http_proxy_port, "/opt/ppaas-configurator-4.1.0-SNAPSHOT/template-modules/wso2as-5.2.1/module.ini")
             p = subprocess.Popen(command, shell=True)
             output, errors = p.communicate()
-            log.info("Successfully updated catalina http proxy port: %s in AS template module" % http_proxy_port)
+            WSO2ASStartupHandler.log.info("Successfully updated catalina http proxy port: %s in AS template module" % http_proxy_port)
 
         if http_proxy_port is not None:
             command = "sed -i \"s/^#CONFIG_PARAM_HTTPS_PROXY_PORT.*/CONFIG_PARAM_HTTPS_PROXY_PORT = %s/g\" %s" % (https_proxy_port, "/opt/ppaas-configurator-4.1.0-SNAPSHOT/template-modules/wso2as-5.2.1/module.ini")
             p = subprocess.Popen(command, shell=True)
             output, errors = p.communicate()
-            log.info("Successfully updated catalina https proxy port: %s in AS template module" % https_proxy_port)
+            WSO2ASStartupHandler.log.info("Successfully updated catalina https proxy port: %s in AS template module" % https_proxy_port)
 
 
         # configure server
-        log.info("Configuring WSO2 AS...")
+        WSO2ASStartupHandler.log.info("Configuring WSO2 AS...")
         config_command = "python /opt/ppaas-configurator-4.1.0-SNAPSHOT/configurator.py"
         env_var = os.environ.copy()
         p = subprocess.Popen(config_command, env=env_var, shell=True)
         output, errors = p.communicate()
-        log.info("WSO2 AS configured successfully")
+        WSO2ASStartupHandler.log.info("WSO2 AS configured successfully")
 
         # start server
-        log.info("Starting WSO2 AS...")
+        WSO2ASStartupHandler.log.info("Starting WSO2 AS...")
 
         start_command = "exec ${CARBON_HOME}/bin/wso2server.sh start"
         env_var = os.environ.copy()
         p = subprocess.Popen(start_command, env=env_var, shell=True)
         output, errors = p.communicate()
-        log.debug("WSO2 AS started successfully")
+        WSO2ASStartupHandler.log.debug("WSO2 AS started successfully")
